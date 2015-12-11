31a8240de87e09457fdecf502c3ba6d4a3bf9b4e
Issac Kelly <issac@issackelly.com>
2015-12-11 19:08:27 +1300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+setup(
+    name = "glass",
+    version = "0.1",
+    packages = find_packages(),
+    scripts = ['glass.py'],
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires = ['requests==2.8.1', 'click==6.2'],
+
+    package_data = {},
+    entry_points={
+          'console_scripts': [
+              'glass = glass:cli'
+          ]
+      },
+
+    # metadata for upload to PyPI
+    author = "Issac Kelly",
+    author_email = "issac@servee.com",
+    description = "Glass CLI",
+)
